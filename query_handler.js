86a5524b64dbdc9030c788b6a372e1518dc1b29a
Alexander Sjösten <sjosten.alexander@gmail.com>
2015-02-01 22:23:16 +0100

    restructure of query_handler to reach "public" methods

--- query_handler.js
+++ query_handler.js
@@ -6,7 +6,8 @@
 function Query() {
     "use strict";
 
-    var api_key, httpGetRequest;
+    // Have queryMovie and queryShow as private as well in order to reach them in queryActor..
+    var api_key, httpGetRequest, queryMovie, queryShow;
 
     // Fill in API key for themoviedb.org here!
     api_key = "";
@@ -29,197 +30,252 @@ function Query() {
         request.send(null);
     };
 
-    return {
-        queryMovie: function (userQuery, callback) {
-            var movies, totalLength;
-            movies = [];
-            totalLength = 0;
+    queryMovie = function (userQuery, movieId, callback) {
+        var movies, totalLength;
+        movies = [];
+        totalLength = 0;
 
-            userQuery = userQuery + "&api_key=" + api_key;
+        userQuery = userQuery + "&api_key=" + api_key;
 
-            function createMovieObjects() {
-                httpGetRequest(userQuery, createMovie);
-            }
-
-            function createMovie(jsonObj) {
-                totalLength = jsonObj.results.length;
-                jsonObj.results.forEach(function (result) {
-                    var id = result.id;
-                    httpGetRequest(
-                        "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + api_key,
-                        function (newJsonObj) {
-                            createMovieFromId(newJsonObj, id);
-                        }
-                    );
-                });
-            }
-
-            function createMovieFromId(jsonObj, movieId) {
-                var title, year, imdbId;
-
-                title = jsonObj.title;
-                year = jsonObj.release_date.substring(0, 4);
-
-                imdbId = jsonObj.imdb_id;
-                if (imdbId !== "") {
-                    httpGetRequest(
-                        "http://www.omdbapi.com/?i=" + imdbId + "&r=json",
-                        function (omdbJsonObj) {
-                            useOMDb(omdbJsonObj, title, year, movieId);
-                        }
-                    );
-                } else {
-                    totalLength--;
-                }
-            }
-
-            function useOMDb(jsonObj, title, year, movieId) {
-                //console.log(jsonObj + "      " + title + "      " + year);
-                httpGetRequest(
-                    "https://api.themoviedb.org/3/movie/" + movieId + "/credits?api_key=" + api_key,
-                    function (newJsonObj) {
-                        getCastAndCrew(
-                            newJsonObj,
-                            title,
-                            year,
-                            jsonObj.imdbRating,
-                            jsonObj.imdbVotes
-                        );
-                    }
-                );
-            }
-
-            function getCastAndCrew(jsonObj, title, year, imdbRating, imdbVotes) {
-                var actors, director, i;
-                //console.log(jsonObj + "    " + title + "    " + year + "    " + imdbRating + "    " + imdbVotes);
-                //console.log(jsonObj.results.crew);
-                actors = [];
-                for (i = 0; i < jsonObj.cast.length; i++) {
-                    actors[i] = jsonObj.cast[i].name;
-                }
-
-                director = "";
-                for (i = 0; i < jsonObj.crew.length; i++) {
-                    if (jsonObj.crew[i].job === "Director") {
-                        director = jsonObj.crew[i].name;
-                        break;
-                    }
-                }
-
-                insertMovie(title, year, actors, director, imdbRating, imdbVotes, "Movie");
-            }
-
-            function insertMovie(title, year, actors, director, imdbRating, imdbVotes, type) {
-                var movie = new Movie(title, year, actors, director, imdbRating, imdbVotes, type);
-                movies.push(movie);
-
-                if (movies.length === totalLength) {
-                    callback(movies);
-                }
-            }
-
-            createMovieObjects();
-        },
-
-        queryActor: function (userQuery) {
-            userQuery = userQuery + "?api_key=" + api_key;
-        },
-
-        queryShow: function (userQuery, callback) {
-            var shows, totalLength;
-
-            shows = [];
-            userQuery = userQuery + "&api_key=" + api_key;
-
-            function getTVShows() {
-                httpGetRequest(userQuery, createTVShow);
-            }
-
-            function createTVShow(jsonObj) {
-                totalLength = jsonObj.results.length;
-                
-                jsonObj.results.forEach(function (result) {
-                    var id = result.id;
-
-                    httpGetRequest(
-                        "https://api.themoviedb.org/3/tv/" + id + "?api_key=" + api_key,
-                        function(newJsonObj) {
-                            createTVShowFromId(newJsonObj, id);
-                        }
-                    ); 
-                });
-            }
-
-            function createTVShowFromId(jsonObj, showId) {
-                var title, startYear, endYear, seasons, episodes, type;
-
-                title = jsonObj.name;
-                if(jsonObj.first_air_date) {
-                    startYear = jsonObj.first_air_date.substring(0, 4);
-                }
-
-                if(jsonObj.last_air_date) {
-                    endYear = jsonObj.last_air_date.substring(0, 4);
-                }
-                
-                status = jsonObj.status;
-                seasons = jsonObj.number_of_seasons;
-                episodes = jsonObj.number_of_episodes;
-                type = "TV Show";
-
-                httpGetRequest(
-                    "https://api.themoviedb.org/3/tv/" + showId + "/credits?api_key=" + api_key,
-                    function(newJsonObj) {
-                        getCastAndCrew(newJsonObj, title, startYear, endYear, status, seasons, episodes, type);
-                    }
-                );
-            }
-
-            function getCastAndCrew(jsonObj, title, startYear, endYear, status, seasons, episodes, type) {
-                var actors, i;
-                
-                actors = [];
-                for (i = 0; i < jsonObj.cast.length; i++) {
-                    actors[i] = jsonObj.cast[i].name;
-                }
-
-                insertShow(title, startYear, endYear, status, seasons, episodes, actors, type);
-            }
-
-            function insertShow(title, startYear, endYear, status, seasons, episodes, actors, type) {
-                var tvShow = new TVShow(title, startYear, endYear, status, seasons, episodes, actors, type);
-                shows.push(tvShow);
-                console.log(shows);
-                if(shows.length === totalLength) {
-                    callback(shows);
-                }
-            }
-
-            getTVShows();
+        function createMovieObjects() {
+            httpGetRequest(userQuery, createMovie);
         }
+
+        function createMovie(jsonObj) {
+            totalLength = jsonObj.results.length;
+            jsonObj.results.forEach(function (result) {
+                var id = result.id;
+                httpGetRequest(
+                    "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + api_key,
+                    function (newJsonObj) {
+                        createMovieFromId(newJsonObj, id);
+                    }
+                );
+            });
+        }
+
+        function createMovieFromId(jsonObj, movieId) {
+            var title, year, imdbId;
+
+            title = jsonObj.title;
+            year = jsonObj.release_date.substring(0, 4);
+
+            imdbId = jsonObj.imdb_id;
+            if (imdbId !== "") {
+                httpGetRequest(
+                    "http://www.omdbapi.com/?i=" + imdbId + "&r=json",
+                    function (omdbJsonObj) {
+                        useOMDb(omdbJsonObj, title, year, movieId);
+                    }
+                );
+            } else {
+                totalLength--;
+            }
+        }
+
+        function useOMDb(jsonObj, title, year, movieId) {
+            //console.log(jsonObj + "      " + title + "      " + year);
+            httpGetRequest(
+                "https://api.themoviedb.org/3/movie/" + movieId + "/credits?api_key=" + api_key,
+                function (newJsonObj) {
+                    getCastAndCrew(
+                        newJsonObj,
+                        title,
+                        year,
+                        jsonObj.imdbRating,
+                        jsonObj.imdbVotes
+                    );
+                }
+            );
+        }
+
+        function getCastAndCrew(jsonObj, title, year, imdbRating, imdbVotes) {
+            var actors, director, i;
+            actors = [];
+            
+            for (i = 0; i < jsonObj.cast.length; i++) {
+                actors[i] = jsonObj.cast[i].name;
+            }
+
+            director = "";
+            for (i = 0; i < jsonObj.crew.length; i++) {
+                if (jsonObj.crew[i].job === "Director") {
+                    director = jsonObj.crew[i].name;
+                    break;
+                }
+            }
+
+            insertMovie(title, year, actors, director, imdbRating, imdbVotes, "Movie");
+        }
+
+        function insertMovie(title, year, actors, director, imdbRating, imdbVotes, type) {
+            var movie = new Movie(title, year, actors, director, imdbRating, imdbVotes, type);
+            movies.push(movie);
+
+            if (movies.length === totalLength) {
+                callback(movies);
+            }
+        }
+
+        if(movieId === undefined) {
+            createMovieObjects();
+        } else {
+            totalLength = 1;
+            httpGetRequest(
+                "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + api_key,
+                function (newJsonObj) {
+                    createMovieFromId(newJsonObj, movieId);
+                }
+            );
+        }
+    }
+
+    queryShow = function (userQuery, serieId, callback) {
+        var shows, totalLength;
+
+        shows = [];
+        userQuery = userQuery + "&api_key=" + api_key;
+
+        function getTVShows() {
+            httpGetRequest(userQuery, createTVShow);
+        }
+
+        function createTVShow(jsonObj) {
+            totalLength = jsonObj.results.length;
+            
+            jsonObj.results.forEach(function (result) {
+                var id = result.id;
+
+                httpGetRequest(
+                    "https://api.themoviedb.org/3/tv/" + id + "?api_key=" + api_key,
+                    function(newJsonObj) {
+                        createTVShowFromId(newJsonObj, id);
+                    }
+                ); 
+            });
+        }
+
+        function createTVShowFromId(jsonObj, showId) {
+            var title, startYear, endYear, seasons, episodes, type;
+
+            title = jsonObj.name;
+            if(jsonObj.first_air_date) {
+                startYear = jsonObj.first_air_date.substring(0, 4);
+            }
+
+            if(jsonObj.last_air_date) {
+                endYear = jsonObj.last_air_date.substring(0, 4);
+            }
+            
+            status = jsonObj.status;
+            seasons = jsonObj.number_of_seasons;
+            episodes = jsonObj.number_of_episodes;
+            type = "TV Show";
+
+            httpGetRequest(
+                "https://api.themoviedb.org/3/tv/" + showId + "/credits?api_key=" + api_key,
+                function(newJsonObj) {
+                    getCastAndCrew(newJsonObj, title, startYear, endYear, status, seasons, episodes, type);
+                }
+            );
+        }
+
+        function getCastAndCrew(jsonObj, title, startYear, endYear, status, seasons, episodes, type) {
+            var actors, i;
+            
+            actors = [];
+            for (i = 0; i < jsonObj.cast.length; i++) {
+                actors[i] = jsonObj.cast[i].name;
+            }
+
+            insertShow(title, startYear, endYear, status, seasons, episodes, actors, type);
+        }
+
+        function insertShow(title, startYear, endYear, status, seasons, episodes, actors, type) {
+            var tvShow = new TVShow(title, startYear, endYear, status, seasons, episodes, actors, type);
+            shows.push(tvShow);
+
+            if(shows.length === totalLength) {
+                callback(shows);
+            }
+        }
+
+        if(serieId === undefined) {
+            getTVShows();
+        } else {
+            totalLength = 1;
+            httpGetRequest(
+                "https://api.themoviedb.org/3/tv/" + serieId + "?api_key=" + api_key,
+                function(newJsonObj) {
+                    createTVShowFromId(newJsonObj, serieId);
+                }
+            );
+        }
+    }
+
+    return {
+        queryMovie: queryMovie,
+
+        queryActor: function (userQuery, callback) {
+            var moviesAndTVShows, callbackResults;
+
+            moviesAndTVShows = [];
+            callbackResults = 0;
+            userQuery = userQuery + "&api_key=" + api_key;
+
+            function doQuery() {
+                console.log(userQuery);
+                httpGetRequest(userQuery, parseActorsFromQuery);
+            }
+            
+            function parseActorsFromQuery(jsonObj) {
+                jsonObj.results.forEach(function (result) {
+                    httpGetRequest(
+                        "http://api.themoviedb.org/3/person/" + result.id + "/movie_credits?api_key=" + api_key,
+                        getAllMovies
+                    );
+
+                    httpGetRequest(
+                        "http://api.themoviedb.org/3/person/" + result.id + "/tv_credits?api_key=" + api_key,
+                        getAllSeries
+                    );
+                });
+            }
+
+            function getAllMovies(jsonObj) {
+                callbackResults += jsonObj.cast.length;
+                jsonObj.cast.forEach(function (movie) {
+                    queryMovie("", movie.id, function (movies) {
+                        movies.forEach(function (movie2) {
+                            moviesAndTVShows.push(movie2);
+                        });
+
+                        if(moviesAndTVShows.length === callbackResults) {
+                            callback(moviesAndTVShows);
+                        }
+                    });
+                });
+            }
+
+            function getAllSeries(jsonObj) {
+                callbackResults += jsonObj.cast.length;
+                jsonObj.cast.forEach(function (serie) {
+                    queryShow("", serie.id, function (series) {
+                        series.forEach(function (serie) {
+                            moviesAndTVShows.push(serie);
+                        });
+
+                        if(moviesAndTVShows.length === callbackResults) {
+                            callback(moviesAndTVShows);
+                        }
+
+                    });
+                });
+            }
+
+            doQuery();
+        },
+
+        queryShow: queryShow
     };
 }
-
-/*
-function queryMovie(q, userCallback) {
-    function stepOne() {
-        sendRequest({
-            success: stepTwo
-        });
-    }
- 
-    function stepTwo() {
-        sendRequest({
-            success: stepThree
-        });
-    }
- 
-    function stepThree() {
-        sendRequest({
-            success: userCallback
-        });
-    }
- 
-    stepOne();
-}
-*/
